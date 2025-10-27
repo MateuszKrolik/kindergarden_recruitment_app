4be0a5c7f47fc083efc88b67876ae34fc8e99f33
Mateusz Królik <mateuszkrolik87@gmail.com>
2025-10-27 17:54:11 +0100

    fix import

--- apps/frontend/app/dashboard/properties/[propertyId]/admin/page.tsx
+++ apps/frontend/app/dashboard/properties/[propertyId]/admin/page.tsx
@@ -1,7 +1,7 @@
 "use server";
 
 import { getAllPropertyChildrenPaged } from "@/app/actions/property-management";
-import { AdminPropertyChildrenTable } from "@/components/client/AdminPropertyChildrenTable";
+import { AdminPropertyChildrenTable } from "@/components/client/property/admin/child/AdminPropertyChildrenTable";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
